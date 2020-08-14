799c3ff66d6e7fc4329bf3117f561a11cfea85a6
Q. Price <priceqp@gmail.com>
2020-08-13 21:29:50 -0500

    implement boolean and select fields on addsnack form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
 
 class AddSnackForm(FlaskForm):
   """Form for adding snacks."""
@@ -7,6 +7,14 @@ class AddSnackForm(FlaskForm):
   name = StringField("Snack Name")
 
   price = FloatField("Price in USD")
+
+  quantity = IntegerField("How Many?")
+
+  is_healthy = BooleanField("This is a healthy snack")
+
+  # category = RadioField("Category", choices=[("ic", "Ice Cream", ), ("chips", "Potato Chips"), ("candy", "Candy/Sweets")])
+
+  category = SelectField("Category", choices=[("ic", "Ice Cream", ), ("chips", "Potato Chips"), ("candy", "Candy/Sweets")])
 
 
 class NewEmployeeForm(FlaskForm):
