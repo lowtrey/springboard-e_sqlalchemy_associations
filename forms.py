bc8b57247fd3c153e8a7888bcdd75d07b39eee6f
Q. Price <priceqp@gmail.com>
2020-08-15 12:21:01 -0500

    implement form error rendering and validation

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
+from wtforms.validators import InputRequired
 
 
 states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
@@ -11,7 +12,7 @@ states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
 class AddSnackForm(FlaskForm):
   """Form for adding snacks."""
 
-  name = StringField("Snack Name")
+  name = StringField("Snack Name", validators=[InputRequired()])
 
   price = FloatField("Price in USD")
 
